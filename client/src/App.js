92312c1619624051fc9673552eeacedb4a67f583
Tushar <reddygtvs@gmail.com>
2022-01-17 10:47:54 +0530

    basic input & button formed

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,20 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      test
+      <div className="gameInfo">
+      <label>Game:</label>
+      <input type="text" />
+      <label>Size(MB):</label>
+      <input type="number" />
+      <label>Publisher:</label>
+      <input type="text" />
+      <label>Region:</label>
+      <input type="text" />
+      <button>Add Game</button>
+      </div>
+
     </div>
   );
 }
